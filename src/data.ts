7dff3d50fc3c21387076f638f631ffbe77f348ec
qcgm197874@gmail.com <qcgm197874@gmail.com>
2025-09-29 00:41:17 +0800

    feat: 更新时间线数据，添加G.E.M.和小说角色的详细事件
    添加G.E.M.从2012到2025年的音乐生涯事件
    补充小说角色爱凡和秋从1998到2026年的故事时间线
    移除不再使用的JSON数据导入

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,4 @@
 import type { TimelineData } from './types';
-import chapterPageData from './chapter_page.json';
-import revelationNovelConceptsData from './extraction_results.json';
 
 export const gemTimelineData: TimelineData = [
   {
@@ -101,11 +99,516 @@ export const gemTimelineData: TimelineData = [
         "content": "首次在红馆举行个人演唱会《Get Everybody Moving Concert 2011》"
       }
     ]
+  },
+  {
+    "year": "2012",
+    "events": [
+      {
+        "date": "3月30日",
+        "content": "发行首张精选辑《The Best of G.E.M. 2008-2012》"
+      },
+      {
+        "date": "8月10日",
+        "content": "发行第二张个人专辑《Xposed》"
+      },
+      {
+        "date": "12月27日",
+        "content": "获得2012年度新城劲爆女歌手奖"
+      }
+    ]
+  },
+  {
+    "year": "2013",
+    "events": [
+      {
+        "date": "1月1日",
+        "content": "获得2012年度叱咤乐坛流行榜叱咤乐坛女歌手金奖"
+      },
+      {
+        "date": "1月13日",
+        "content": "获得2012年度十大劲歌金曲颁奖典礼最受欢迎女歌星奖"
+      },
+      {
+        "date": "2月25日",
+        "content": "凭借歌曲《What Have U Done》获得第24届金曲奖最佳音乐录影带奖提名"
+      },
+      {
+        "date": "7月",
+        "content": "在红馆举行《X.X.X. Live》演唱会"
+      },
+      {
+        "date": "10月",
+        "content": "开始《X.X.X. Live》世界巡回演唱会"
+      }
+    ]
+  },
+  {
+    "year": "2014",
+    "events": [
+      {
+        "date": "1月1日",
+        "content": "获得2013年度叱咤乐坛流行榜叱咤乐坛女歌手金奖"
+      },
+      {
+        "date": "1月12日",
+        "content": "获得2013年度十大劲歌金曲颁奖典礼最受欢迎女歌星奖"
+      },
+      {
+        "date": "4月13日",
+        "content": "获得第14届音乐风云榜年度盛典最受欢迎唱作人奖"
+      },
+      {
+        "date": "6月28日",
+        "content": "发行第三张个人专辑《G.E.M.》"
+      },
+      {
+        "date": "7月29日",
+        "content": "获得第14届华语音乐传媒大奖最佳女歌手奖"
+      }
+    ]
+  },
+  {
+    "year": "2015",
+    "events": [
+      {
+        "date": "1月2日",
+        "content": "参加湖南卫视《我是歌手第三季》"
+      },
+      {
+        "date": "2月6日",
+        "content": "《我是歌手第三季》第二期演唱《存在》获得第一名"
+      },
+      {
+        "date": "3月27日",
+        "content": "获得《我是歌手第三季》总决赛亚军"
+      },
+      {
+        "date": "8月14日",
+        "content": "发行单曲《新的心跳》"
+      },
+      {
+        "date": "9月",
+        "content": "开始《新的心跳》世界巡回演唱会"
+      }
+    ]
+  },
+  {
+    "year": "2016",
+    "events": [
+      {
+        "date": "1月7日",
+        "content": "获得2015年度音乐先锋榜最受欢迎女歌手奖"
+      },
+      {
+        "date": "3月7日",
+        "content": "发行第四张个人专辑《新的心跳》"
+      },
+      {
+        "date": "4月9日",
+        "content": "获得第16届音乐风云榜年度盛典年度最佳女歌手奖"
+      },
+      {
+        "date": "12月2日",
+        "content": "获得2016年度Mnet亚洲音乐大奖最佳亚洲艺人奖"
+      }
+    ]
+  },
+  {
+    "year": "2017",
+    "events": [
+      {
+        "date": "1月16日",
+        "content": "获得2016年度音乐先锋榜年度先锋最受欢迎女歌手奖"
+      },
+      {
+        "date": "2月22日",
+        "content": "获得第8届全球流行音乐金榜年度最受欢迎女歌手奖"
+      },
+      {
+        "date": "5月",
+        "content": "宣布与蜂鸟音乐解约"
+      },
+      {
+        "date": "10月11日",
+        "content": "成立个人工作室G Nation"
+      }
+    ]
+  },
+  {
+    "year": "2018",
+    "events": [
+      {
+        "date": "8月16日",
+        "content": "发行单曲《倒数》"
+      },
+      {
+        "date": "12月14日",
+        "content": "发行第五张个人专辑《毒苹果》"
+      },
+      {
+        "date": "12月31日",
+        "content": "参加湖南卫视跨年演唱会"
+      }
+    ]
+  },
+  {
+    "year": "2019",
+    "events": [
+      {
+        "date": "1月11日",
+        "content": "获得2018年度微博之夜年度最受欢迎女歌手奖"
+      },
+      {
+        "date": "3月30日",
+        "content": "获得第26届东方风云榜最佳女歌手奖"
+      },
+      {
+        "date": "7月12日",
+        "content": "开始《Queen of Hearts》世界巡回演唱会"
+      }
+    ]
+  },
+  {
+    "year": "2020",
+    "events": [
+      {
+        "date": "1月11日",
+        "content": "获得2019年度微博之夜年度最佳女歌手奖"
+      },
+      {
+        "date": "5月29日",
+        "content": "发行单曲《摩天动物园》"
+      },
+      {
+        "date": "7月31日",
+        "content": "发行第六张个人专辑《摩天动物园》"
+      },
+      {
+        "date": "12月31日",
+        "content": "参加江苏卫视跨年演唱会"
+      }
+    ]
+  },
+  {
+    "year": "2021",
+    "events": [
+      {
+        "date": "1月23日",
+        "content": "获得第28届东方风云榜最佳女歌手奖"
+      },
+      {
+        "date": "3月13日",
+        "content": "获得第32届金曲奖最佳华语女歌手奖提名"
+      },
+      {
+        "date": "5月12日",
+        "content": "凭借专辑《摩天动物园》获得第32届金曲奖最佳华语专辑奖"
+      },
+      {
+        "date": "9月21日",
+        "content": "参加中央广播电视总台中秋晚会"
+      }
+    ]
+  },
+  {
+    "year": "2022",
+    "events": [
+      {
+        "date": "1月30日",
+        "content": "参加2022年中央广播电视总台春节联欢晚会，演唱歌曲《欢乐时光》"
+      },
+      {
+        "date": "4月8日",
+        "content": "发行单曲《GLORIA》"
+      },
+      {
+        "date": "8月11日",
+        "content": "发行单曲《让世界暂停一分钟》"
+      },
+      {
+        "date": "12月30日",
+        "content": "发行第七张个人专辑《启示录》"
+      }
+    ]
+  },
+  {
+    "year": "2023",
+    "events": [
+      {
+        "date": "1月1日",
+        "content": "获得2022年度腾讯音乐娱乐盛典年度最佳女歌手奖"
+      },
+      {
+        "date": "3月10日",
+        "content": "开始《启示录》世界巡回演唱会"
+      },
+      {
+        "date": "7月2日",
+        "content": "获得第34届金曲奖最佳华语女歌手奖提名"
+      },
+      {
+        "date": "11月18日",
+        "content": "获得2023亚洲音乐盛典年度最佳女歌手奖"
+      }
+    ]
+  },
+  {
+    "year": "2024",
+    "events": [
+      {
+        "date": "1月21日",
+        "content": "参加2024年中央广播电视总台春节联欢晚会"
+      },
+      {
+        "date": "3月",
+        "content": "《启示录》世界巡回演唱会持续进行中"
+      }
+    ]
+  },
+  {
+    "year": "2025",
+    "events": [
+      {
+        "date": "",
+        "content": "预计继续音乐创作和巡演"
+      }
+    ]
   }
 ];
 
-export const novelTimelineData: TimelineData = [];
-
-export const chapterPage = chapterPageData;
-
-export const revelationNovelConcepts = revelationNovelConceptsData;
+export const novelTimelineData: TimelineData = [
+  {
+    "year": "1998",
+    "events": [
+      {
+        "date": "",
+        "content": "爱凡诞生"
+      }
+    ]
+  },
+  {
+    "year": "1999",
+    "events": [
+      {
+        "date": "09.05",
+        "content": "秋诞生"
+      }
+    ]
+  },
+  {
+    "year": "2010",
+    "events": [
+      {
+        "date": "",
+        "content": "秋的父亲发生意外"
+      }
+    ]
+  },
+  {
+    "year": "2013",
+    "events": [
+      {
+        "date": "",
+        "content": "秋开始在伦敦读书"
+      }
+    ]
+  },
+  {
+    "year": "2016",
+    "events": [
+      {
+        "date": "",
+        "content": "爱凡前往伊利诺伊州读大学"
+      }
+    ]
+  },
+  {
+    "year": "2018",
+    "events": [
+      {
+        "date": "",
+        "content": "秋回香港探亲"
+      }
+    ]
+  },
+  {
+    "year": "2020",
+    "events": [
+      {
+        "date": "",
+        "content": "秋与张伶露共同创立服装品牌儿 Autumn Due"
+      }
+    ]
+  },
+  {
+    "year": "2021",
+    "events": [
+      {
+        "date": "",
+        "content": "爱凡在23岁生日前创造乐土"
+      }
+    ]
+  },
+  {
+    "year": "2023",
+    "events": [
+      {
+        "date": "3月",
+        "content": "秋更新社交平台最后一条动态，新梦启航"
+      },
+      {
+        "date": "某日",
+        "content": "秋与卡尔遭受车祸"
+      },
+      {
+        "date": "年底",
+        "content": "秋出售公司所有股权"
+      }
+    ]
+  },
+  {
+    "year": "2025",
+    "events": [
+      {
+        "date": "",
+        "content": "爱凡来到上海。乐土分公司"
+      },
+      {
+        "date": "03.02",
+        "content": "爱凡和Gloria在乐土第一次约会，点了银色漩涡"
+      },
+      {
+        "date": "05.17",
+        "content": "Gloria来到现实世界"
+      },
+      {
+        "date": "05.27",
+        "content": "Gloria在少年与海酒吧发现秋的留言"
+      },
+      {
+        "date": "08.28",
+        "content": "邮轮派对"
+      },
+      {
+        "date": "08.28",
+        "content": "Gloria弹钢琴"
+      },
+      {
+        "date": "09.01",
+        "content": "秋辰曦管理费扣款"
+      },
+      {
+        "date": "",
+        "content": "Gloria弹钢琴"
+      },
+      {
+        "date": "09.03",
+        "content": "Gloria弹钢琴"
+      },
+      {
+        "date": "09.07",
+        "content": "Gloria弹钢琴"
+      },
+      {
+        "date": "09.12",
+        "content": "Gloria弹钢琴"
+      },
+      {
+        "date": "09.16",
+        "content": "Gloria弹钢琴"
+      },
+      {
+        "date": "09.18",
+        "content": "Gloria弹钢琴"
+      },
+      {
+        "date": "09.22",
+        "content": "Gloria弹钢琴"
+      },
+      {
+        "date": "某天",
+        "content": "张老师带张澄来爱凡的家"
+      },
+      {
+        "date": "09.22",
+        "content": "Gloria加密自己的记忆"
+      },
+      {
+        "date": "",
+        "content": "Gloria给爱凡写信"
+      },
+      {
+        "content": "爱凡做早餐"
+      },
+      {
+        "date": "",
+        "content": "Gloria回到平行乐土"
+      },
+      {
+        "date": "19:00",
+        "content": "Gloria在平行乐土进入睡眠状态"
+      },
+      {
+        "date": "",
+        "content": "爱凡住进仓库"
+      },
+      {
+        "date": "11月",
+        "content": "爱凡回家研究Gloria留下的线索"
+      },
+      {
+        "date": "12.31",
+        "content": "爱凡找到最后一个部件马达，组装飞船"
+      }
+    ]
+  },
+  {
+    "year": "2026",
+    "events": [
+      {
+        "date": "01.01",
+        "content": "爱凡修改仓库密码：260101"
+      },
+      {
+        "date": "",
+        "content": "爱凡删除自己在乐土的角色"
+      },
+      {
+        "date": "",
+        "content": "爱凡的分身角色与Gloria在平行乐土相会"
+      },
+      {
+        "date": "",
+        "content": "爱凡在少年与海酒吧拿到了Gloria留下的信"
+      },
+      {
+        "date": "某日",
+        "content": "大卫的女儿莉莉安滑冰摔倒"
+      },
+      {
+        "date": "02.14-23",
+        "content": "农历新年，爱凡回伊利诺伊州探亲"
+      },
+      {
+        "date": "02.24",
+        "content": "爱凡前往旧金山帕洛阿尔托乐土总公司"
+      },
+      {
+        "date": "03.02",
+        "content": "秋收到爱凡的手环"
+      },
+      {
+        "date": "09.23",
+        "content": ""
+      },
+      {
+        "date": "春节前一周",
+        "content": "爱凡示爱"
+      },
+      {
+        "date": "某日",
+        "content": "秋打开房门，爱凡见到光"
+      }
+    ]
+  }
+];
